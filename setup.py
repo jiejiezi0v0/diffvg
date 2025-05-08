057c4dbab461451ee9ec30db32133fdf0869c632
jiejiezi0v0 <108184153+jiejiezi0v0@users.noreply.github.com>
2025-05-08 15:55:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,8 +36,9 @@ class Build(build_ext):
             info = get_paths()
             include_path = info['include']
             cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                          '-DPYTHON_LIBRARY=' + get_config_var('LIBDIR'),
-                          '-DPYTHON_INCLUDE_PATH=' + include_path]
+                        '-DPYTHON_LIBRARY=' + get_config_var('LIBDIR'),
+                        '-DPYTHON_INCLUDE_PATH=' + include_path,
+                        '-DPYTHON_INCLUDE_DIR=' + include_path]
 
             cfg = 'Debug' if self.debug else 'Release'
             build_args = ['--config', cfg]
